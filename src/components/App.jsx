32a421f7ea893179b98dd43cd55ecaeaac809fc5
hirichardhai <hirichardhai@gmail.com>
2018-10-22 14:21:01 -0700

    added basic design layout for App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,24 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        App Component
+      <div className="App">
+        <div className="title">
+          Todo List Reminder
+        </div>
+        <div className="form-inline">
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="I have to..."
+            />
+          </div>
+          <button
+            type="button"
+            className="btn btn-success"
+          >
+            Add Reminder
+          </button>
+        </div>
       </div>
     )
   }
