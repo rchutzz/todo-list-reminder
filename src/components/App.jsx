fe6fc4cade414fb5f276d9360ce7c088c59b4f37
hirichardhai <hirichardhai@gmail.com>
2018-10-22 20:07:37 -0700

    successfully connected App.jsx with global state to update state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('this.props', this.props);
     return (
       <div className="App">
         <div className="title">
@@ -41,4 +42,10 @@ class App extends Component {
   }
 }
 
-export default connect(null, { addReminder })(App);
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, { addReminder })(App);
