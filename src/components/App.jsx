33f607365f2a87ca495044ad4bf2569f56e21119
Richard Hai <hirichardhai@gmail.com>
2018-10-23 02:54:08 -0700

    added calendar input field that updates apps state to new date selected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      dueDate: ''
     }
   }
 
@@ -53,6 +54,11 @@ class App extends Component {
               className="form-control"
               placeholder="I have to..."
               onChange={event => this.setState({text: event.target.value})}
+            />
+            <input
+              className="form-control"
+              type="datetime-local"
+              onChange={event => this.setState({dueDate: event.target.value})}
             />
           </div>
           <button
