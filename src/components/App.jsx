5f6ad2931f42c330c707d00aaaf76b5127078d62
hirichardhai <hirichardhai@gmail.com>
2018-10-22 19:24:20 -0700

    added state to App.jsx that takes in the input value and logs the state when clicking the button tag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    console.log('this.state', this.state);
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,11 +23,13 @@ class App extends Component {
             <input
               className="form-control"
               placeholder="I have to..."
+              onChange={event => this.setState({text: event.target.value})}
             />
           </div>
           <button
             type="button"
             className="btn btn-success"
+            onClick={() => this.addReminder()}
           >
             Add Reminder
           </button>
